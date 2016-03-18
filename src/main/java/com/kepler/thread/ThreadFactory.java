7cd6d5ff204e6b56832350bb4c55ca2a4f9ae2df
shenjiawei <shenjiawei@didichuxing.com>
2016-03-18 11:23:43 +0800

    修复Headers在同一个RPC上下文中多次发送Request时丢失

--- src/main/java/com/kepler/thread/ThreadFactory.java
+++ src/main/java/com/kepler/thread/ThreadFactory.java
@@ -17,18 +17,13 @@ public class ThreadFactory implements FactoryBean<ThreadPoolExecutor> {
 
 	private static final Log LOGGER = LogFactory.getLog(ThreadFactory.class);
 
-	/**
-	 * 最小线程数量
-	 */
-	private static final int THREAD_MIN = 3;
+	private static final int THREAD_MAX = PropertiesUtils.get(ThreadFactory.class.getName().toLowerCase() + ".max", Runtime.getRuntime().availableProcessors() * 2);
 
-	private static final int THREAD_CORE = PropertiesUtils.get(ThreadFactory.class.getName().toLowerCase() + ".core", ThreadFactory.THREAD_MIN + Runtime.getRuntime().availableProcessors() * 2);
-
-	private static final int THREAD_MAX = PropertiesUtils.get(ThreadFactory.class.getName().toLowerCase() + ".max", ThreadFactory.THREAD_CORE);
-
-	private static final int THREAD_KEEPALIVE = PropertiesUtils.get(ThreadFactory.class.getName().toLowerCase() + ".keepalive", 60000);
+	private static final int THREAD_CORE = PropertiesUtils.get(ThreadFactory.class.getName().toLowerCase() + ".core", Runtime.getRuntime().availableProcessors() + 1);
 
 	private static final int THREAD_QUEUE = PropertiesUtils.get(ThreadFactory.class.getName().toLowerCase() + ".queue", 50);
+
+	private static final int THREAD_KEEPALIVE = PropertiesUtils.get(ThreadFactory.class.getName().toLowerCase() + ".keepalive", 60000);
 
 	/**
 	 * 是否使用ShutdownNow
