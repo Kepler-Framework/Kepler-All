fe77c4040d6d9f8dd244848075927dac28ba38bc
shenjiawei <shenjiawei@didichuxing.com>
2016-08-02 14:50:40 +0800

    增加Enums泛化

---
+++ src/main/java/com/kepler/generic/arg/EnumArg.java
@@ -0,0 +1,36 @@
+package com.kepler.generic.arg;
+
+import com.kepler.KeplerGenericException;
+import com.kepler.generic.GenericArg;
+import com.kepler.org.apache.commons.lang.reflect.MethodUtils;
+
+/**
+ * @author KimShen
+ *
+ */
+public class EnumArg implements GenericArg {
+
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * 对象类型
+	 */
+	private final String clazz;
+
+	private final String value;
+
+	public EnumArg(String clazz, String value) {
+		super();
+		this.clazz = clazz;
+		this.value = value;
+	}
+
+	@Override
+	public Object arg() throws KeplerGenericException {
+		try {
+			return MethodUtils.invokeStaticMethod(Class.forName(this.clazz), "valueOf", this.value);
+		} catch (Exception e) {
+			throw new KeplerGenericException(e);
+		}
+	}
+}
