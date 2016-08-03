f7b09e0e303228fd1d6d0531202f30990764210c
shenjiawei <shenjiawei@didichuxing.com>
2016-08-03 18:15:31 +0800

    组合Jsr303异常

--- src/main/java/com/kepler/protocol/validation/Jsr303Validation.java
+++ src/main/java/com/kepler/protocol/validation/Jsr303Validation.java
@@ -1,10 +1,11 @@
 package com.kepler.protocol.validation;
 
+import java.util.Set;
+
 import javax.validation.ConstraintViolation;
 import javax.validation.Validation;
 import javax.validation.Validator;
 
-import com.kepler.KeplerLocalException;
 import com.kepler.KeplerValidateException;
 import com.kepler.protocol.Request;
 import com.kepler.protocol.RequestValidation;
@@ -23,12 +24,28 @@ public class Jsr303Validation implements RequestValidation {
 		this.support = support;
 	}
 
+	/**
+	 * 组合异常并抛出
+	 * 
+	 * @param exceptions
+	 */
+	private void appendAndThrow(Set<ConstraintViolation<Object>> exceptions) {
+		StringBuffer buffer = new StringBuffer();
+		for (ConstraintViolation<Object> valid : exceptions) {
+			// [属性路径1=异常信息] [属性路径2=异常信息]
+			buffer.append("[").append(valid.getPropertyPath()).append("=").append(valid.getMessage() + "]");
+		}
+		throw new KeplerValidateException(buffer.toString());
+	}
+
 	@Override
 	public Request valid(Request request) throws KeplerValidateException {
 		for (Object arg : request.args()) {
 			if (arg != null) {
-				for (ConstraintViolation<Object> valid : Jsr303Validation.VALIDATOR.validate(arg)) {
-					throw new KeplerLocalException(valid.getMessage());
+				Set<ConstraintViolation<Object>> exceptions = Jsr303Validation.VALIDATOR.validate(arg);
+				// 如果存在异常则开始拼装
+				if (!exceptions.isEmpty()) {
+					this.appendAndThrow(exceptions);
 				}
 			}
 		}
