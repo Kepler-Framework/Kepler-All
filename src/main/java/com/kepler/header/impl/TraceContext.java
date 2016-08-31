9364fbfa12149a47346f9b9777a502758144711b
shenjiawei <shenjiawei@didichuxing.com>
2016-08-31 12:26:20 +0800

    TraceContext获取同时增加Log4J.MDC

--- src/main/java/com/kepler/header/impl/TraceContext.java
+++ src/main/java/com/kepler/header/impl/TraceContext.java
@@ -2,9 +2,13 @@ package com.kepler.header.impl;
 
 import java.util.UUID;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.util.StringUtils;
 
+import com.kepler.config.PropertiesUtils;
 import com.kepler.header.Headers;
+import com.kepler.org.apache.commons.lang.reflect.MethodUtils;
 import com.kepler.trace.Trace;
 
 /**
@@ -16,6 +20,29 @@ import com.kepler.trace.Trace;
 public class TraceContext {
 
 	/**
+	 * 是否开启Log4j.MDC
+	 */
+	private static final boolean MDC = PropertiesUtils.get(TraceContext.class.getName().toLowerCase() + ".log4j_mdc", false);
+
+	private static final Log LOGGER = LogFactory.getLog(TraceContext.class);
+
+	/**
+	 * Log4j.MDC
+	 */
+	private static Class<?> CLASS;
+
+	static {
+		// 加载Log4j.MDC
+		if (TraceContext.MDC) {
+			try {
+				TraceContext.CLASS = Class.forName("org.apache.log4j.MDC");
+			} catch (Exception e) {
+				TraceContext.LOGGER.warn(e.getMessage(), e);
+			}
+		}
+	}
+
+	/**
 	 * 获取上下文相关Trace, 如果不存在则创建. 如果未开启Header则返回Null
 	 * 
 	 * @return
@@ -24,6 +51,7 @@ public class TraceContext {
 		if (Headers.ENABLED) {
 			Headers headers = ThreadHeaders.HEADERS.get();
 			String trace = headers.get(Trace.TRACE);
+			// 如果Trace不存在则创建Trace
 			return StringUtils.isEmpty(trace) ? TraceContext.generate(headers) : trace;
 		} else {
 			return null;
@@ -39,7 +67,23 @@ public class TraceContext {
 	private static String generate(Headers headers) {
 		// 使用UUID创建Trace
 		String trace = UUID.randomUUID().toString();
-		headers.put(Trace.TRACE, trace);
+		headers.put(Trace.TRACE, TraceContext.log4jmdc(trace));
+		return trace;
+	}
+
+	/**
+	 * 开启MDC
+	 * 
+	 * @param trace
+	 */
+	private static String log4jmdc(String trace) {
+		try {
+			if (TraceContext.CLASS != null) {
+				MethodUtils.invokeStaticMethod(TraceContext.CLASS, "put", new Object[] { Trace.TRACE, trace });
+			}
+		} catch (Exception e) {
+			TraceContext.LOGGER.error(e.getMessage(), e);
+		}
 		return trace;
 	}
 }
