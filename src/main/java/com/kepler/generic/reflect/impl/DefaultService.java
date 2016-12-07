c4bb06027ac252b9a064314e2392c2e4566bafc6
BluesZhang <zhangjiehao@didichuxing.com>
2016-12-07 14:51:46 +0800

    泛化调用用GenericSerial

--- src/main/java/com/kepler/generic/reflect/impl/DefaultService.java
+++ src/main/java/com/kepler/generic/reflect/impl/DefaultService.java
@@ -1,6 +1,5 @@
 package com.kepler.generic.reflect.impl;
 
-import com.kepler.config.Profile;
 import com.kepler.config.PropertiesUtils;
 import com.kepler.generic.GenericMarker;
 import com.kepler.generic.impl.DefaultImported;
@@ -11,8 +10,8 @@ import com.kepler.header.HeadersProcessor;
 import com.kepler.id.IDGenerators;
 import com.kepler.invoker.Invoker;
 import com.kepler.protocol.RequestFactory;
-import com.kepler.serial.SerialID;
 import com.kepler.serial.Serials;
+import com.kepler.serial.generic.GenericSerial;
 import com.kepler.service.Imported;
 import com.kepler.service.Service;
 
@@ -34,11 +33,8 @@ public class DefaultService extends DefaultImported implements GenericService {
 
 	private static final Object[] EMPTY = new Object[] { null };
 
-	private final Profile profile;
-
-	public DefaultService(HeadersProcessor processor, IDGenerators generators, RequestFactory factory, HeadersContext header, GenericMarker marker, Profile profile, Imported imported, Serials serials, Invoker invoker) {
+	public DefaultService(HeadersProcessor processor, IDGenerators generators, RequestFactory factory, HeadersContext header, GenericMarker marker, Imported imported, Serials serials, Invoker invoker) {
 		super(processor, generators, factory, header, marker, imported, serials, invoker);
-		this.profile = profile;
 	}
 
 	@Override
@@ -47,10 +43,9 @@ public class DefaultService extends DefaultImported implements GenericService {
 			// 尝试Import服务(如果未注册)
 			super.imported(service);
 		}
-		byte serial = this.serials.output(PropertiesUtils.profile(this.profile.profile(service), SerialID.SERIAL_KEY, SerialID.SERIAL_VAL));
 		// 获取Header并标记为泛型(隐式开启Header)
 		Headers headers = super.marker.mark(super.processor.process(service, super.header.get()));
 		// 强制同步调用
-		return super.invoker.invoke(super.factory.request(headers, service, method, false, new Object[] { new DelegateArgs(classes, args != null ? args : DefaultService.EMPTY) }, DefaultService.CLASSES, super.generators.get(service, method).generate(), serial));
+		return super.invoker.invoke(super.factory.request(headers, service, method, false, new Object[] { new DelegateArgs(classes, args != null ? args : DefaultService.EMPTY) }, DefaultService.CLASSES, super.generators.get(service, method).generate(), GenericSerial.SERIAL));
 	}
 }
