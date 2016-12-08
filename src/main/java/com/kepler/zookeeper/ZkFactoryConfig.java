361696d61b2e77613547c9e4c12d0aa1da1c2ffd
shenjiawei <shenjiawei@didichuxing.com>
2016-12-08 14:28:33 +0800

    更新ZooKeeper reset方法

--- src/main/java/com/kepler/zookeeper/ZkFactoryConfig.java
+++ src/main/java/com/kepler/zookeeper/ZkFactoryConfig.java
@@ -1,5 +1,11 @@
 package com.kepler.zookeeper;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import com.kepler.annotation.Async;
 import com.kepler.annotation.Config;
 import com.kepler.annotation.Internal;
 
@@ -12,6 +18,10 @@ public class ZkFactoryConfig {
 
 	public static final String RESET_KEY = "com.kepler.zookeeper.zkfactoryconfig.reset";
 
+	private static final Log LOGGER = LogFactory.getLog(ZkFactoryConfig.class);
+
+	private final AtomicInteger version = new AtomicInteger();
+
 	private final ZkFactory factory;
 
 	public ZkFactoryConfig(ZkFactory factory) {
@@ -20,10 +30,19 @@ public class ZkFactoryConfig {
 	}
 
 	/**
-	 * 最大重试次数内重试
+	 * 仅当指定版本等于内置版本时触发重置
+	 * 
+	 * @param version
 	 */
 	@Config(value = ZkFactoryConfig.RESET_KEY)
-	public void reset(String operator) {
-		this.factory.reset(operator);
+	@Async
+	public void reset(int version) {
+		// 如果请求版本小于当前版本则不进行更新
+		if (this.version.get() == version) {
+			this.version.getAndIncrement();
+			this.factory.reset();
+		} else {
+			ZkFactoryConfig.LOGGER.warn("Unvalid version [current=" + this.version.get() + "][request=" + version + "]");
+		}
 	}
 }
