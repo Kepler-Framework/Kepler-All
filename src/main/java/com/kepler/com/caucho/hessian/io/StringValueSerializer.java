f017ba4ec494685db6df40ce4db5c0d7280c18f8
shenjiawei <shenjiawei@didichuxing.com>
2017-11-24 14:28:14 +0800

    Bug fixed, 修复Hessian单独使用无法加载BigDecimalDeserializer

--- src/main/java/com/kepler/com/caucho/hessian/io/StringValueSerializer.java
+++ src/main/java/com/kepler/com/caucho/hessian/io/StringValueSerializer.java
@@ -53,36 +53,54 @@ import java.io.IOException;
 /**
  * Serializing a remote object.
  */
-public class StringValueSerializer extends AbstractSerializer {
-  public static final Serializer SER = new StringValueSerializer();
-  
-  public void writeObject(Object obj, AbstractHessianOutput out)
-    throws IOException
-  {
-    if (obj == null)
-      out.writeNull();
-    else {
-      if (out.addRef(obj))
-        return;
-      
-      Class cl = obj.getClass();
+public class StringValueSerializer extends AbstractSerializer implements com.caucho.hessian.io.Serializer {
 
-      int ref = out.writeObjectBegin(cl.getName());
+	public static final Serializer SER = new StringValueSerializer();
 
-      if (ref < -1) {
-        out.writeString("value");
-        out.writeString(obj.toString());
-        out.writeMapEnd();
-      }
-      else {
-        if (ref == -1) {
-          out.writeInt(1);
-          out.writeString("value");
-          out.writeObjectBegin(cl.getName());
-        }
+	public void writeObject(Object obj, AbstractHessianOutput out) throws IOException {
+		if (obj == null)
+			out.writeNull();
+		else {
+			if (out.addRef(obj))
+				return;
+			Class<?> cl = obj.getClass();
+			int ref = out.writeObjectBegin(cl.getName());
+			if (ref < -1) {
+				out.writeString("value");
+				out.writeString(obj.toString());
+				out.writeMapEnd();
+			} else {
+				if (ref == -1) {
+					out.writeInt(1);
+					out.writeString("value");
+					out.writeObjectBegin(cl.getName());
+				}
+				out.writeString(obj.toString());
+			}
+		}
+	}
 
-        out.writeString(obj.toString());
-      }
-    }
-  }
+	@Override
+	public void writeObject(Object obj, com.caucho.hessian.io.AbstractHessianOutput out) throws IOException {
+		if (obj == null)
+			out.writeNull();
+		else {
+			if (out.addRef(obj))
+				return;
+			Class<?> cl = obj.getClass();
+			int ref = out.writeObjectBegin(cl.getName());
+			if (ref < -1) {
+				out.writeString("value");
+				out.writeString(obj.toString());
+				out.writeMapEnd();
+			} else {
+				if (ref == -1) {
+					out.writeInt(1);
+					out.writeString("value");
+					out.writeObjectBegin(cl.getName());
+				}
+				out.writeString(obj.toString());
+			}
+		}
+	}
 }
