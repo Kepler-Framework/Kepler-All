692aafcda3b8e9bf85d4889b3bc8c7556542c73e
shenjiawei <shenjiawei@didichuxing.com>
2017-11-30 17:23:29 +0800

    增加KeplerMockException

---
+++ src/main/java/com/kepler/KeplerMockException.java
@@ -0,0 +1,14 @@
+package com.kepler;
+
+/**
+ * @author KimShen
+ *
+ */
+public class KeplerMockException extends KeplerRemoteException {
+
+	private static final long serialVersionUID = 1L;
+
+	public KeplerMockException(String e) {
+		super(e);
+	}
+}
